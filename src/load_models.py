7d6f9c3b6ce030bfba7990a3e162c9cc8f2f53fd
Shrut Sureja <92169549+shrutsureja@users.noreply.github.com>
2023-09-21 17:53:12 +0530

    cleaning in the localGPT repo

--- src/load_models.py
+++ src/load_models.py
@@ -1,3 +1,4 @@
+
 import torch
 from auto_gptq import AutoGPTQForCausalLM
 from huggingface_hub import hf_hub_download
@@ -9,15 +10,17 @@ from transformers import (
     LlamaForCausalLM,
     LlamaTokenizer,
 )
-from constants import CONTEXT_WINDOW_SIZE, MAX_NEW_TOKENS, N_GPU_LAYERS, N_BATCH, MODELS_PATH
-
+from constants import (
+    CONTEXT_WINDOW_SIZE, 
+    MAX_NEW_TOKENS
+)
 
 def load_quantized_model_gguf_ggml(model_id, model_basename, device_type, logging):
     """
     Load a GGUF/GGML quantized model using LlamaCpp.
 
-    This function attempts to load a GGUF/GGML quantized model using the LlamaCpp library.
-    If the model is of type GGML, and newer version of LLAMA-CPP is used which does not support GGML,
+    This function attempts to load a GGUF/GGML quantized model using the LlamaCpp library. 
+    If the model is of type GGML, and newer version of LLAMA-CPP is used which does not support GGML, 
     it logs a message indicating that LLAMA-CPP has dropped support for GGML.
 
     Parameters:
@@ -37,25 +40,28 @@ def load_quantized_model_gguf_ggml(model_id, model_basename, device_type, loggin
     try:
         logging.info("Using Llamacpp for GGUF/GGML quantized models")
         model_path = hf_hub_download(
-            repo_id=model_id,
-            filename=model_basename,
-            resume_download=True,
-            cache_dir=MODELS_PATH,
-        )
-        model_path = ""
+            repo_id=model_id, 
+            filename=model_basename, 
+            resume_download=True, 
+            cache_dir="./models",
+            )
+        logging.info(f"Model : {model_id} is downloaded and ready to use");
         kwargs = {
             "model_path": model_path,
             "n_ctx": CONTEXT_WINDOW_SIZE,
             "max_tokens": MAX_NEW_TOKENS,
-            "n_batch": N_BATCH,  # set this based on your GPU & CPU RAM
         }
         if device_type.lower() == "mps":
             kwargs["n_gpu_layers"] = 1
         if device_type.lower() == "cuda":
-            kwargs["n_gpu_layers"] = N_GPU_LAYERS  # set this based on your GPU
-
-        return LlamaCpp(**kwargs)
+            kwargs["n_gpu_layers"] = 100 # set this based on your GPU
+        logging.info(f"Model : {model_id} returend to its parent function");
+        logging.info(f"Model path : {model_path}");
+        llm = LlamaCpp(
+            model_path=model_path,
+        )
+        return llm
     except:
-        if "ggml" in model_basename:
-            logging.INFO("If you were using GGML model, LLAMA-CPP Dropped Support, Use GGUF Instead")
+        if 'ggml' in model_basename:
+            logging.info("If you were using GGML model, LLAMA-CPP Dropped Support, Use GGUF Instead")
         return None
